a00a5848aa00afd6bdc025a7908de2a77b07ea0a
Krzysztof Swend <krzysiekswend@gmail.com>
2023-03-01 20:50:26 +0100

    Usprawnienie kodu
    Stworzenie nowej klasy do tworzenia takich samych "kontenerów".
    Dodanie przycisku na samym dole

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+const double bottomContainerHeight = 80;
+const activeCardColor = Color(0xFF1D1E33);
+const bottomContainerColour = Color(0xFFEB1555);
+
 class InputPage extends StatefulWidget {
   const InputPage({Key? key}) : super(key: key);
 
@@ -20,60 +24,51 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: [
                 Expanded(
-                  child: Container(
-                    margin: EdgeInsets.all(15),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Color(0xFF1D1E33),
-                    ),
-                  ),
+                  child: ReusableCode(color: activeCardColor),
                 ),
-                Expanded(
-                  child: Container(
-                    margin: EdgeInsets.all(15),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Color(0xFF1D1E33),
-                    ),
-                  ),
-                ),
+                Expanded(child: ReusableCode(color: activeCardColor)),
               ],
             ),
           ),
           Expanded(
-            child: Container(
-              margin: EdgeInsets.all(15),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Color(0xFF1D1E33),
-              ),
-            ),
+            child: ReusableCode(color: activeCardColor),
           ),
           Expanded(
             child: Row(
               children: [
                 Expanded(
-                  child: Container(
-                    margin: EdgeInsets.all(15),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Color(0xFF1D1E33),
-                    ),
-                  ),
+                  child: ReusableCode(color: activeCardColor),
                 ),
                 Expanded(
-                  child: Container(
-                    margin: EdgeInsets.all(15),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Color(0xFF1D1E33),
-                    ),
-                  ),
+                  child: ReusableCode(color: activeCardColor),
                 ),
               ],
             ),
+          ),
+          Container(
+            color: bottomContainerColour,
+            margin: EdgeInsets.only(top: 10),
+            width: double.infinity,
+            height: bottomContainerHeight,
           )
         ],
+      ),
+    );
+  }
+}
+
+class ReusableCode extends StatelessWidget {
+  ReusableCode({required this.color});
+
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: color,
       ),
     );
   }
