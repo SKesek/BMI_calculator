0d671c7b154d3eead96891f52528b853b133637d
Krzysztof Swend <krzysiekswend@gmail.com>
2023-03-02 21:22:20 +0100

    Kafelki wyboru płci
    Dodanie ikonek oraz tekstu, stworzenie plików do tworzenia "kart" oraz ikon, stworzenie funkcji która jest odpowiedzialna za zmianę koloru podczas wyboru płci

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'reusable_card.dart';
+import 'icon_content.dart';
 
 const double bottomContainerHeight = 80;
 const activeCardColor = Color(0xFF1D1E33);
+const inactiveCardColor = Color(0xFF111328);
 const bottomContainerColour = Color(0xFFEB1555);
 
 class InputPage extends StatefulWidget {
@@ -12,6 +16,28 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  Color maleCardColour = inactiveCardColor;
+  Color femaleCardColour = inactiveCardColor;
+  //1 - male, 2 - female
+  void updateColour(int gender) {
+    if (gender == 1) {
+      if (maleCardColour == inactiveCardColor) {
+        maleCardColour = activeCardColor;
+        femaleCardColour = inactiveCardColor;
+      } else {
+        maleCardColour = inactiveCardColor;
+      }
+    }
+    if (gender == 2) {
+      if (femaleCardColour == inactiveCardColor) {
+        femaleCardColour = activeCardColor;
+        maleCardColour = inactiveCardColor;
+      } else {
+        femaleCardColour = inactiveCardColor;
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,51 +50,67 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: [
                 Expanded(
-                  child: ReusableCode(color: activeCardColor),
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        updateColour(1);
+                      });
+                    },
+                    child: ReusableCode(
+                      color: maleCardColour,
+                      cardChild: const IconContent(
+                        icon: FontAwesomeIcons.mars,
+                        label: 'MALE',
+                      ),
+                    ),
+                  ),
                 ),
-                Expanded(child: ReusableCode(color: activeCardColor)),
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        updateColour(2);
+                      });
+                    },
+                    child: ReusableCode(
+                      color: femaleCardColour,
+                      cardChild: const IconContent(
+                        icon: FontAwesomeIcons.venus,
+                        label: 'FEMALE',
+                      ),
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
           Expanded(
-            child: ReusableCode(color: activeCardColor),
+            child: ReusableCode(
+              color: activeCardColor,
+              cardChild: Container(),
+            ),
           ),
           Expanded(
             child: Row(
               children: [
                 Expanded(
-                  child: ReusableCode(color: activeCardColor),
+                  child: ReusableCode(
+                      color: activeCardColor, cardChild: Container()),
                 ),
                 Expanded(
-                  child: ReusableCode(color: activeCardColor),
+                  child: ReusableCode(
+                      color: activeCardColor, cardChild: Container()),
                 ),
               ],
             ),
           ),
           Container(
             color: bottomContainerColour,
-            margin: EdgeInsets.only(top: 10),
+            margin: const EdgeInsets.only(top: 10),
             width: double.infinity,
             height: bottomContainerHeight,
           )
         ],
-      ),
-    );
-  }
-}
-
-class ReusableCode extends StatelessWidget {
-  ReusableCode({required this.color});
-
-  final Color color;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(15),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        color: color,
       ),
     );
   }
---
+++ lib/reusable_card.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ReusableCode extends StatelessWidget {
+  ReusableCode({required this.color, required this.cardChild});
+
+  final Color color;
+  final Widget cardChild;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: color,
+      ),
+      child: cardChild,
+    );
+  }
+}
